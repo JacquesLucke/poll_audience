1dfd48154d88b90a65e4f559ef68d61eb3bdb129
Jacques Lucke <mail@jlucke.com>
2024-08-26 18:54:41 +0200

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use actix_cors::Cors;
-use actix_web::http::header::{CacheControl, CacheDirective};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::http::header::{CacheControl, CacheDirective, ContentType};
+use actix_web::http::StatusCode;
+use actix_web::{error, get, post, web, App, HttpResponse, HttpServer, Responder};
 use clap::Parser;
+use derive_more::derive::{Display, Error};
 use std::collections::HashMap;
 use std::sync::Mutex;
 
@@ -15,7 +17,64 @@ struct Args {
     port: u16,
 }
 
-type SessionID = String;
+#[derive(Debug, Display, Error)]
+enum AppError {
+    EmptySessionID,
+    TooLongSessionID,
+    SessionIDDoesNotExist,
+    ServerError,
+    EmptyUserID,
+    TooLongUserID,
+}
+
+impl error::ResponseError for AppError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match *self {
+            AppError::EmptySessionID => StatusCode::BAD_REQUEST,
+            AppError::TooLongSessionID => StatusCode::BAD_REQUEST,
+            AppError::SessionIDDoesNotExist => StatusCode::BAD_REQUEST,
+            AppError::ServerError => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::EmptyUserID => StatusCode::BAD_REQUEST,
+            AppError::TooLongUserID => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct SessionID(String);
+
+impl SessionID {
+    fn from_string(s: &str) -> Result<SessionID, AppError> {
+        if s.is_empty() {
+            Err(AppError::EmptySessionID)
+        } else if s.len() > 100 {
+            Err(AppError::TooLongSessionID)
+        } else {
+            Ok(SessionID(s.to_string()))
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize)]
+struct UserID(String);
+
+impl UserID {
+    fn from_string(s: &str) -> Result<UserID, AppError> {
+        if s.is_empty() {
+            Err(AppError::EmptyUserID)
+        } else if s.len() > 100 {
+            Err(AppError::TooLongUserID)
+        } else {
+            Ok(UserID(s.to_string()))
+        }
+    }
+}
 
 struct AppState {
     sessions: Mutex<Sessions>,
@@ -27,7 +86,7 @@ struct Sessions {
 
 struct SessionState {
     page_content: String,
-    response_by_user: HashMap<String, String>,
+    response_by_user: HashMap<UserID, String>,
 }
 
 #[get("/")]
@@ -36,15 +95,19 @@ async fn index() -> String {
 }
 
 #[get("/{session_id}")]
-async fn page_for_session(path: web::Path<String>, state: web::Data<AppState>) -> impl Responder {
-    let session_id = path.into_inner();
-    let sessions = state.sessions.lock().unwrap();
-    match sessions.state_by_id.get(&session_id) {
-        None => HttpResponse::NotFound().body("Unknown Session\n"),
-        Some(session) => HttpResponse::Ok()
-            .insert_header(CacheControl(vec![CacheDirective::NoCache]))
-            .body(session.page_content.clone()),
-    }
+async fn page_for_session(
+    path: web::Path<String>,
+    state: web::Data<AppState>,
+) -> Result<impl Responder, AppError> {
+    let session_id = SessionID::from_string(&path.into_inner())?;
+    let sessions = state.sessions.lock().map_err(|_| AppError::ServerError)?;
+    let session = sessions
+        .state_by_id
+        .get(&session_id)
+        .ok_or(AppError::SessionIDDoesNotExist)?;
+    Ok(HttpResponse::Ok()
+        .insert_header(CacheControl(vec![CacheDirective::NoCache]))
+        .body(session.page_content.clone()))
 }
 
 #[post("/{session_id}/set_page")]
@@ -52,9 +115,9 @@ async fn set_page(
     body: String,
     path: web::Path<String>,
     state: web::Data<AppState>,
-) -> impl Responder {
-    let session_id = path.into_inner();
-    let mut sessions = state.sessions.lock().unwrap();
+) -> Result<impl Responder, AppError> {
+    let session_id = SessionID::from_string(&path.into_inner())?;
+    let mut sessions = state.sessions.lock().map_err(|_| AppError::ServerError)?;
     let session = sessions
         .state_by_id
         .entry(session_id)
@@ -64,20 +127,23 @@ async fn set_page(
         });
     session.page_content = body;
     session.response_by_user.clear();
-    "Page Updated\n"
+    Ok(HttpResponse::Ok())
 }
 
 #[post("/{session_id}/reset_responses")]
-async fn reset(path: web::Path<String>, state: web::Data<AppState>) -> impl Responder {
-    let session_id = path.into_inner();
-    let mut sessions = state.sessions.lock().unwrap();
+async fn reset(
+    path: web::Path<String>,
+    state: web::Data<AppState>,
+) -> Result<impl Responder, AppError> {
+    let session_id = SessionID::from_string(&path.into_inner())?;
+    let mut sessions = state.sessions.lock().map_err(|_| AppError::ServerError)?;
     match sessions.state_by_id.get_mut(&session_id) {
-        None => HttpResponse::Ok(),
+        None => {}
         Some(session) => {
             session.response_by_user.clear();
-            HttpResponse::Ok()
         }
     }
+    Ok(HttpResponse::Ok())
 }
 
 #[post("/{session_id}/respond/{user_id}")]
@@ -85,28 +151,33 @@ async fn respond(
     body: String,
     path: web::Path<(String, String)>,
     state: web::Data<AppState>,
-) -> impl Responder {
+) -> Result<impl Responder, AppError> {
     let (session_id, user_id) = path.into_inner();
-    let mut sessions = state.sessions.lock().unwrap();
-    match sessions.state_by_id.get_mut(&session_id) {
-        None => HttpResponse::NotFound().body("Unknown Session\n"),
-        Some(session) => {
-            session.response_by_user.insert(user_id, body);
-            HttpResponse::Ok().body("Responded\n")
-        }
-    }
+    let session_id = SessionID::from_string(&session_id)?;
+    let user_id = UserID::from_string(&user_id)?;
+    let mut sessions = state.sessions.lock().map_err(|_| AppError::ServerError)?;
+    let session = sessions
+        .state_by_id
+        .get_mut(&session_id)
+        .ok_or(AppError::SessionIDDoesNotExist)?;
+    session.response_by_user.insert(user_id, body);
+    Ok(HttpResponse::Ok())
 }
 
 #[get("/{session_id}/responses")]
-async fn responses(path: web::Path<String>, state: web::Data<AppState>) -> impl Responder {
-    let session_id = path.into_inner();
-    let sessions = state.sessions.lock().unwrap();
-    match sessions.state_by_id.get(&session_id) {
-        None => HttpResponse::NotFound().json(Vec::<String>::new()),
-        Some(session) => HttpResponse::Ok()
-            .insert_header(CacheControl(vec![CacheDirective::NoCache]))
-            .json(&session.response_by_user),
-    }
+async fn responses(
+    path: web::Path<String>,
+    state: web::Data<AppState>,
+) -> Result<impl Responder, AppError> {
+    let session_id = SessionID::from_string(&path.into_inner())?;
+    let sessions = state.sessions.lock().map_err(|_| AppError::ServerError)?;
+    let session = sessions
+        .state_by_id
+        .get(&session_id)
+        .ok_or(AppError::SessionIDDoesNotExist)?;
+    Ok(HttpResponse::Ok()
+        .insert_header(CacheControl(vec![CacheDirective::NoCache]))
+        .json(&session.response_by_user))
 }
 
 #[actix_web::main]
