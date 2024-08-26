d5fdd8c1dfcc338f47ce86814c7cc0748bf41a23
Jacques Lucke <mail@jlucke.com>
2024-08-26 19:50:48 +0200

    add basic garbage collection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use actix_cors::Cors;
 use actix_web::http::header::{CacheControl, CacheDirective, ContentType};
 use actix_web::http::StatusCode;
+use actix_web::web::PayloadConfig;
 use actix_web::{error, get, post, web, App, HttpResponse, HttpServer, Responder};
+use chrono::{DateTime, Utc};
 use clap::Parser;
 use derive_more::derive::{Display, Error};
 use std::collections::HashMap;
@@ -25,6 +27,7 @@ enum AppError {
     ServerError,
     EmptyUserID,
     TooLongUserID,
+    PageTooLarge,
 }
 
 impl error::ResponseError for AppError {
@@ -42,6 +45,7 @@ impl error::ResponseError for AppError {
             AppError::ServerError => StatusCode::INTERNAL_SERVER_ERROR,
             AppError::EmptyUserID => StatusCode::BAD_REQUEST,
             AppError::TooLongUserID => StatusCode::BAD_REQUEST,
+            AppError::PageTooLarge => StatusCode::PAYLOAD_TOO_LARGE,
         }
     }
 }
@@ -87,6 +91,7 @@ struct Sessions {
 struct SessionState {
     page_content: String,
     response_by_user: HashMap<UserID, String>,
+    last_update: DateTime<Utc>,
 }
 
 #[get("/")]
@@ -116,6 +121,9 @@ async fn set_page(
     path: web::Path<String>,
     state: web::Data<AppState>,
 ) -> Result<impl Responder, AppError> {
+    if body.len() > 1_000_000 {
+        return Err(AppError::PageTooLarge);
+    }
     let session_id = SessionID::from_string(&path.into_inner())?;
     let mut sessions = state.sessions.lock().map_err(|_| AppError::ServerError)?;
     let session = sessions
@@ -124,7 +132,9 @@ async fn set_page(
         .or_insert_with(|| SessionState {
             page_content: "".into(),
             response_by_user: HashMap::new(),
+            last_update: Utc::now(),
         });
+    session.last_update = Utc::now();
     session.page_content = body;
     session.response_by_user.clear();
     Ok(HttpResponse::Ok())
@@ -161,6 +171,7 @@ async fn respond(
         .get_mut(&session_id)
         .ok_or(AppError::SessionIDDoesNotExist)?;
     session.response_by_user.insert(user_id, body);
+    session.last_update = Utc::now();
     Ok(HttpResponse::Ok())
 }
 
@@ -180,6 +191,23 @@ async fn responses(
         .json(&session.response_by_user))
 }
 
+async fn periodically_clear_old_sessions(state: web::Data<AppState>) {
+    let interval = tokio::time::Duration::from_secs(60 * 60);
+    let session_life_time = chrono::Duration::seconds(24 * 60 * 60);
+
+    let mut interval = tokio::time::interval(interval);
+    loop {
+        interval.tick().await;
+        let mut sessions = state.sessions.lock().unwrap();
+        sessions.state_by_id.retain(|_, session| {
+            let current_time = Utc::now();
+            let last_update_time = session.last_update;
+            let is_old = current_time > last_update_time + session_life_time;
+            !is_old
+        });
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
@@ -190,8 +218,15 @@ async fn main() -> std::io::Result<()> {
         }),
     });
     println!("Start server on http://{}:{}", args.host, args.port);
+
+    let state_clone = state.clone();
+    tokio::spawn(async {
+        periodically_clear_old_sessions(state_clone).await;
+    });
+
     HttpServer::new(move || {
         App::new()
+            .app_data(PayloadConfig::new(1 * 1024 * 1024))
             .wrap(Cors::permissive())
             .app_data(state.clone())
             .service(index)
